559e098f9167af016995175caf8d531a2b735cf6
leosdcn <98526791+leosdcn@users.noreply.github.com>
2026-06-08 23:11:48 +0800

    Site updated: 2026-06-08 23:11:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/07/这是一篇新的博文/","2026/06/07/hello-world/"];function toRandomPost(){
+var posts=["posts/hello-world.html","posts/这是一篇新的博文.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
